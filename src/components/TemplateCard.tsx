33d8f21dbc296b2bacef0d131892324751f6fe4c
sedhha <activity.schoolsh2@gmail.com>
2023-06-03 22:38:20 +0530

    Updated Template Card

--- src/components/TemplateCard.tsx
+++ src/components/TemplateCard.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 import classes from './TemplateCard.module.css';
+import Link from 'next/link';
 
 const TemplateCard = ({ src, alt }: { src: string; alt: string }) => (
 	<div className={classes.card}>
-		<Image
-			src='/sample.png'
-			width={500}
-			height={500}
-			alt='Picture of the author'
-		/>
+		<Link href={'/templates/classic/classic-bnw'}>
+			<Image
+				src='/sample.png'
+				width={500}
+				height={500}
+				alt='Picture of the author'
+			/>
+		</Link>
 	</div>
 );
 
